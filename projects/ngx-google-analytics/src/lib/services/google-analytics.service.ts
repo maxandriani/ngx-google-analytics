37a82fc998844e1b68b12afa594f3c881bb44c79
Victor Mafra <vlmmafra@gmail.com>
2019-01-22 17:10:53 -0200

    Created methods to track pageviews and Google Analytics Events

---
+++ projects/ngx-google-analytics/src/lib/services/google-analytics.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, Inject } from '@angular/core';
+import { GaActionEnum } from '../enums/ga-action.enum';
+import { NGX_GOOGLE_ANALYTICS_SETTINGS_TOKEN } from '../tokens/ngx-google-analytics-settings-token';
+import { IGoogleAnalyticsSettings } from '../interfaces/i-google-analytics-settings';
+@Injectable({
+  providedIn: 'root'
+})
+export class GoogleAnalyticsService {
+
+  constructor(
+    @Inject(NGX_GOOGLE_ANALYTICS_SETTINGS_TOKEN) protected $trackingId: IGoogleAnalyticsSettings
+  ) { }
+
+  event(action: GaActionEnum | string, category?: string, label?: string, value?: string) {
+    if (window['gtag']) {
+      const options = {};
+      if (category) {
+        options['category'] = category;
+      }
+      if (label) {
+        options['label'] = label;
+      }
+      if (value) {
+        options['value'] = value;
+      }
+      window['gtag']('event', action, options);
+    }
+  }
+
+  pageView(title?: string, location?: string, path?: string) {
+    if (window['gtag']) {
+      const options = {};
+      if (title) {
+        options['title'] = title;
+      }
+      if (location) {
+        options['location'] = location;
+      }
+      if (path) {
+        options['path'] = path;
+      }
+      window['gtag']('config', this.$trackingId, options);
+    }
+  }
+}

a8e3c57a98a1c0559a63884c212fdead3cd0abe6
SamanthaAdrichem <samantha@adrichem.nu>
2021-01-04 15:32:59 +0100

    Using enum instead of string type (#38)
    Co-authored-by: Samantha Adrichem <s.adrichem@daisycon.com>

--- projects/ngx-google-analytics/src/lib/directives/ga-event.directive.ts
+++ projects/ngx-google-analytics/src/lib/directives/ga-event.directive.ts
@@ -2,7 +2,7 @@ import { Directive, Optional, Input, HostListener, OnInit, isDevMode, Inject } f
 import { GaEventCategoryDirective } from './ga-event-category.directive';
 import { GoogleAnalyticsService } from '../services/google-analytics.service';
 import { GaBind } from '../types/ga-bind.type';
-import { GaAction } from '../types/ga-action.type';
+import { GaActionEnum } from '../enums/ga-action.enum';
 import { NGX_GOOGLE_ANALYTICS_SETTINGS_TOKEN } from '../tokens/ngx-google-analytics-settings-token';
 import { IGoogleAnalyticsSettings } from '../interfaces/i-google-analytics-settings';
 
@@ -19,13 +19,13 @@ export class GaEventDirective implements OnInit {
   ) {
   }
 
-  @Input() gaAction: GaAction | string;
+  @Input() gaAction: GaActionEnum | string;
   @Input() gaLabel: string;
   @Input() label: string;
   @Input() gaValue: number;
   @Input() gaInteraction: boolean;
   @Input() gaBind: GaBind = 'click';
-  @Input() gaEvent: GaAction | string;
+  @Input() gaEvent: GaActionEnum | string;
 
   ngOnInit() {
   }
@@ -59,7 +59,7 @@ export class GaEventDirective implements OnInit {
       // }
 
       if (!this.gaAction && !this.gaEvent) {
-        throw new Error('You must provide a gaAction atrribute to identify this event.');
+        throw new Error('You must provide a gaAction attribute to identify this event.');
       }
 
       this.gaService
